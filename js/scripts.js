7b769dda1e6d1934e4b2e6bc56959aff86825cf6
Paolo-Turchioe <pao.turchioe@gmail.com>
2018-02-20 23:14:13 -0500

    Wine change

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,71 @@
-var defaultCenter = [40.8169169,-73.918108];
-var defaultZoom = 14;
-var map = L.map('my-map').setView(defaultCenter, defaultZoom);
 
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
-    attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-    subdomains: 'abcd',
+var map = L.map('my-map').setView([37.8, -96], 4);
+
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 1,
+	maxZoom: 16,
+	ext: 'png'
 }).addTo(map);
 
-const lookupYearBuilt = function(YearBuilt) {
-  switch(YearBuilt){
-    case '2017':
-      return{
+L.geoJson(statesData).addTo(map);
+
+function getColor(d) {
+    return d > 50000000  ? '#BD0026' :
+           d > 20000000  ? '#E31A1C' :
+           d > 1000000  ? '#FC4E2A' :
+           d > 500000   ? '#FD8D3C' :
+           d > 2000   ? '#FEB24C' :
+           d > 0   ? '#FED976' :
+                      '#FFEDA0';
+}
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.gallon),
+        weight: 2,
+        opacity: 1,
         color: 'white',
-      }
-    }
-    }
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
 
-var plutodataGeojson = L.geoJSON(plutodata, {
-  style: function(feature){
+L.geoJson(statesData, {style: style}).addTo(map);
 
-    return{
-      color: 'white',
-      fillColor: lookupYearBuilt(feature.properties.YearBuilt).color,
-      fillOpacity:0.8,
-      weight:1,
-    }
-},
-onEachFeature: function(feature, layer){
-  const description= lookupYearBuilt(feature.properties.YearBuilt).description;
+function highlightFeature(e) {
+    var layer = e.target;
 
-  layer.bindPopup('${feature.properties.Address}<br/>${description}',{
-    closeButton: false,
-    minWidth:60,
-    offset:[0,-10]
-  });
-  layer.on('mouseover',function(e){
-    this.openPopup();
-
-    e.target.setSTyle({
-      weight:3,
-      color: 'white',
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
     });
 
-    if(!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-      layer.bringToFront();
-    }
-  });
-  layer.on('mouseout', function(e){
-    this.closePopup();
-    plutodataGeojson.resetStyle(e.target);
-  });
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
 }
-}).addto(map);
+
+var geojson;
+
+geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
